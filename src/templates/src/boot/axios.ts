4dc136f3137c3925812d9cc0f6a2ba8b79722fd0
MrBaoquan <mrma617@gmail.com>
2021-09-28 11:03:48 +0800

    compitable with v2

--- src/templates/src/boot/axios.ts
+++ src/templates/src/boot/axios.ts
@@ -1,56 +1,56 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { boot } from "quasar/wrappers";
-import axios, { AxiosError, AxiosInstance } from "axios";
-import qs from "qs";
+import { boot } from 'quasar/wrappers';
+import axios, { AxiosError, AxiosInstance } from 'axios';
+import qs from 'qs';
 
-declare module "@vue/runtime-core" {
-    interface ComponentCustomProperties {
-        $axios: AxiosInstance;
-    }
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $axios: AxiosInstance;
+  }
 }
 
 const axiosInstance: AxiosInstance = axios.create({
-    paramsSerializer: (params) => {
-        return qs.stringify(params, { arrayFormat: "repeat" });
-    },
-    baseURL: "http://quasarapp.com",
+  paramsSerializer: (params) => {
+    return qs.stringify(params, { arrayFormat: 'repeat' });
+  },
+  baseURL: '<%= prompts.apiServer %>',
 });
 
 const setErrorInterceptor = (errorFunction: () => void) => {
-    axiosInstance.interceptors.response.use(
-        (response) => {
-            return response;
-        },
-        (error: AxiosError) => {
-            if (!error.response) {
-                errorFunction();
-            }
-            return Promise.reject(error);
-        }
-    );
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error: AxiosError) => {
+      if (!error.response) {
+        errorFunction();
+      }
+      return Promise.reject(error);
+    }
+  );
 };
 
 // 设置baseURL
 const setBaseURL = (baseURL: string) => {
-    axiosInstance.defaults.baseURL = baseURL;
+  axiosInstance.defaults.baseURL = baseURL;
 };
 
 export { axiosInstance, setErrorInterceptor, setBaseURL };
 export default boot(({ app, store }) => {
-    axiosInstance.interceptors.response.use(
-        function (response) {
-            return response;
-        },
-        function (error) {
-            // Any status codes that falls outside the range of 2xx cause this function to trigger
-            // Do something with response error
-            if (error.response.status === 401) {
-                store.dispatch("qhper/redirect2Auth").catch(null);
-            }
-            return Promise.reject(error);
-        }
-    );
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    app.config.globalProperties.$axios = axiosInstance;
+  axiosInstance.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      if (error.response.status === 401) {
+        store.dispatch('qhper/redirect2Auth').catch(null);
+      }
+      return Promise.reject(error);
+    }
+  );
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  app.config.globalProperties.$axios = axiosInstance;
 });
--- src/templates/src/boot/qhper.ts
+++ src/templates/src/boot/qhper.ts
@@ -1,34 +1,34 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { boot } from "quasar/wrappers";
+import { boot } from 'quasar/wrappers';
 
 export interface User {
-    id: number;
-    email: string;
-    username: string;
-    avatar_url: string;
+  id: number;
+  email: string;
+  username: string;
+  avatar_url: string;
 }
 
 export interface QHper {
-    user: () => User;
+  user: () => User;
 }
 
-declare module "@vue/runtime-core" {
-    interface ComponentCustomProperties {
-        $qhper: QHper;
-    }
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $qhper: QHper;
+  }
 }
 
 export default boot(({ app, store }) => {
-    // 组织qhper实例
-    const qhper: QHper = {
-        user: () => store.getters["qhper/user"],
-    };
+  // 组织qhper实例
+  const qhper: QHper = {
+    user: () => store.getters['qhper/user'],
+  };
 
-    // 组织用户基本信息
-    store.commit("qhper/assembleUser", (data: User) => data);
+  // 组织用户基本信息
+  store.commit('qhper/assembleUser', (data: User) => data);
 
-    // 注入qhper实例
-    app.config.globalProperties.$qhper = qhper;
+  // 注入qhper实例
+  app.config.globalProperties.$qhper = qhper;
 });

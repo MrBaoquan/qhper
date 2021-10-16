92be3a9631573dd2b1ac88357a6e0f84677dfebe
mrbaoquan <mrma617@gmail.com>
2021-10-16 13:44:06 +0800

    增加qhper 类型定义.

--- src/core/store/getters.js
+++ src/core/store/getters.js
@@ -5,3 +5,26 @@ export const loggedIn = (state) => {
 export const user = (state)=>{
     return state.user;
 }
+
+export const roles = (state)=>{
+    const roles = state.user.roles;
+    if(!Array.isArray(roles)||roles.length<=0) return [];
+    return roles.map(_role=>_role.name);
+}
+
+export const check = (state,getters)=> roles=>{
+    const user = getters.user
+    if(user){
+        const userRoles = getters.roles;
+        if(Array.isArray(roles)&&roles.length){
+            for(const role of roles){
+                if(!userRoles.includes(role)) return false;
+            }
+        }else if(roles){
+            if(!userRoles.includes(roles)) return false;
+        }
+        return true;
+    }
+    return false
+    
+}
--- src/templates/src/boot/qhper.ts
+++ src/templates/src/boot/qhper.ts
@@ -4,31 +4,41 @@
 import { boot } from 'quasar/wrappers';
 
 export interface User {
-  id: number;
-  email: string;
-  username: string;
-  avatar_url: string;
+    id: number;
+    name: string;
+    phone: string;
+    email: string;
+    username: string;
+    avatar_url: string;
+    roles: [];
 }
 
 export interface QHper {
-  user: () => User;
+    user: () => User;
+    check: (roles: string[] | string) => boolean;
+}
+
+export interface QHperStateInterface {
+    user: User;
+    check: (roles: string[] | string) => boolean;
 }
 
 declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $qhper: QHper;
-  }
+    interface ComponentCustomProperties {
+        $qhper: QHper;
+    }
 }
 
 export default boot(({ app, store }) => {
-  // 组织qhper实例
-  const qhper: QHper = {
-    user: () => store.getters['qhper/user'],
-  };
+    // 组织qhper实例
+    const qhper: QHper = {
+        user: () => store.getters['qhper/user'],
+        check: () => store.getters['qhper/check'],
+    };
 
-  // 组织用户基本信息
-  store.commit('qhper/assembleUser', (data: User) => data);
+    // 组织用户基本信息
+    store.commit('qhper/assembleUser', (data: User) => data);
 
-  // 注入qhper实例
-  app.config.globalProperties.$qhper = qhper;
+    // 注入qhper实例
+    app.config.globalProperties.$qhper = qhper;
 });
